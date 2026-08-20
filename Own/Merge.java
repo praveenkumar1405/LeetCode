9d7040c64c322ba57202e90de44def51ae0e07ff
praveenkumar1405 <praveenleomessi11@gmail.com>
2026-08-20 10:09:41 +0530

    Implement merge function to combine two arrays

---
+++ Own/Merge.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int result[] = new int [m+n];
+        for(int i=0;i<m;i++)
+        {
+            result[i]=nums1[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            result[m+i]=nums2[i];
+        }
+        Arrays.sort(result);
+        for(int i=0;i<m+n;i++)
+        {
+            nums1[i]=result[i];
+        }
+    }
+}
